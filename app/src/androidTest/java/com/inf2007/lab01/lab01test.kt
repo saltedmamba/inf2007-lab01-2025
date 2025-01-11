91e818c9649347ef593daa893c992c27192b724b
saltedmamba <jarrygoh01@gmail.com>
2025-01-11 22:14:20 +0800

    edited the variable name, testTag and some codes.

--- app/src/androidTest/java/com/inf2007/lab01/lab01test.kt
+++ app/src/androidTest/java/com/inf2007/lab01/lab01test.kt
@@ -11,7 +11,7 @@ import androidx.compose.ui.test.performTextInput
 import org.junit.Rule
 import org.junit.Test
 
-class lab01test {
+class Lab01test {
     @get:Rule
     val composeTestRule = createComposeRule()
 
